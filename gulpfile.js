dbe62c04752728e03617ed3facfaec0157edefbf
GrimdvlDesktop <grimdvl@gmail.com>
2023-12-14 01:43:07 +0300

    forms

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
 const dist = "./dist/";
+// const dist = "C:/OSPanel/domains/projects/javascript-pro/art/dist";
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
