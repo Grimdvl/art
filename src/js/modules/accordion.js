4e46baca6411324b9b0d28fec4f289c4997ed29e
GrimdvlLaptop <grimdvl@gmail.com>
2023-12-19 21:05:50 +0200

    creating accordeon

---
+++ src/js/modules/accordion.js
@@ -0,0 +1,34 @@
+const accordion = (triggerSelector, itemsSelector) => {
+    const btns = document.querySelectorAll(triggerSelector),
+          blocks = document.querySelectorAll(itemsSelector);
+
+    btns.forEach(btn => {
+        btn.addEventListener('click', function() {
+            this.classList.toggle('active-style');
+            this.nextElementSibling.classList.toggle('active-content');
+
+            if (this.classList.contains('active-style')) {
+                
+            } else {
+                
+            }
+        });
+    });
+
+    // blocks.forEach(block => {
+    //     block.classList.add('animated', 'fadeInDown');
+    // });
+
+    // btns.forEach(btn => {
+    //     btn.addEventListener('click', function() {
+    //         if (!this.classList.contains('active')) {
+    //             btns.forEach(btn => {
+    //                 btn.classList.remove('active', 'active-style');
+    //             });
+    //             this.classList.add('active', 'active-style');
+    //         }
+    //     });
+    // });
+};
+
+export default accordion;
